7b4b2872fc8e4d99434ba82a7641578567389cd4
Thibault Jouan <tj@a13.fr>
2013-07-28 16:01:10 +0000

    Replace autoload with explicit require calls

--- lib/producer/core.rb
+++ lib/producer/core.rb
@@ -1,8 +1,3 @@
+require 'producer/core/cli'
+require 'producer/core/recipe'
 require 'producer/core/version'
-
-module Producer
-  module Core
-    autoload :CLI,    'producer/core/cli'
-    autoload :Recipe, 'producer/core/recipe'
-  end
-end

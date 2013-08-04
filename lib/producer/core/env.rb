0cccbc8dde6d793c8d2f589414cee5ade9743caf
Thibault Jouan <tj@a13.fr>
2013-08-04 20:24:19 +0000

    Add Env#output with $stdout as default

--- lib/producer/core/env.rb
+++ lib/producer/core/env.rb
@@ -2,11 +2,17 @@ module Producer
   module Core
     class Env
       attr_reader   :current_recipe
+      attr_writer   :output
       attr_accessor :target
 
       def initialize(recipe = nil)
         @current_recipe = recipe
+        @output         = $stdout
         @target         = nil
+      end
+
+      def output(str)
+        @output.puts str
       end
     end
   end
--- spec/producer/core/env_spec.rb
+++ spec/producer/core/env_spec.rb
@@ -13,6 +13,10 @@ module Producer::Core
         it 'has no recipe' do
           expect(env.current_recipe).not_to be
         end
+
+        it 'assigns $stdout as the default output' do
+          expect(env.instance_eval { @output} ).to eq $stdout
+        end
       end
 
       context 'when a recipe is given as argument' do
@@ -22,6 +26,16 @@ module Producer::Core
         it 'assigns the current recipe' do
           expect(env.current_recipe).to eq recipe
         end
+      end
+    end
+
+    describe '#output' do
+      let(:standard_output) { StringIO.new }
+
+      it 'writes the given string to the assigned IO with a record separator' do
+        env.output = standard_output
+        expect(standard_output).to receive(:puts).with('some content')
+        env.output 'some content'
       end
     end
 

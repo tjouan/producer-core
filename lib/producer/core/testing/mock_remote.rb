85e3bf2eac84610778d178750599dd0a377772d5
Thibault Jouan <tj@a13.fr>
2014-03-04 05:40:13 +0000

    Implement Testing::MockRemote#execute

--- lib/producer/core/testing/mock_remote.rb
+++ lib/producer/core/testing/mock_remote.rb
@@ -5,6 +5,20 @@ module Producer
         def session
           raise 'no session for mock remote!'
         end
+
+        def execute(command)
+          tokens = command.split
+          program = tokens.shift
+
+          case program
+          when 'echo'
+            tokens.join ' '
+          when 'true'
+            ''
+          when 'false'
+            raise RemoteCommandExecutionError
+          end
+        end
       end
     end
   end
--- spec/producer/core/testing/mock_remote_spec.rb
+++ spec/producer/core/testing/mock_remote_spec.rb
@@ -1,3 +1,4 @@
+require 'spec_helper'
 require 'producer/core/testing'
 
 module Producer::Core
@@ -12,6 +13,32 @@ module Producer::Core
       describe '#session' do
         it 'raises an error to prevent real session usage' do
           expect { remote.session }.to raise_error
+        end
+      end
+
+      describe '#execute' do
+        context 'dummy echo command' do
+          let(:command) { 'echo some arguments' }
+
+          it 'returns command arguments' do
+            expect(remote.execute(command)).to eq 'some arguments'
+          end
+        end
+
+        context 'dummy true command' do
+          let(:command) { 'true' }
+
+          it 'returns an empty string' do
+            expect(remote.execute(command)).to eq ''
+          end
+        end
+
+        context 'dummy false command' do
+          let(:command) { 'false' }
+
+          it 'raises a RemoteCommandExecutionError' do
+            expect { remote.execute(command) }.to raise_error(RemoteCommandExecutionError)
+          end
         end
       end
     end

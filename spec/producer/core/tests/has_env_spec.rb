25b1394178d84a5f653d8856938d672750f222db
Thibault Jouan <tj@a13.fr>
2014-04-24 18:38:58 +0000

    Improve HasEnv specs:
    * Use a more classic style, and rely on a simple Hash instead of
      stubbing a double;
    * Improve examples to show the intent more clearly (like the use of a
      symbol when querying).

--- spec/producer/core/tests/has_env_spec.rb
+++ spec/producer/core/tests/has_env_spec.rb
@@ -4,36 +4,37 @@ module Producer::Core
   module Tests
     describe HasEnv do
       let(:env)           { Env.new }
-      let(:variable_name) { :some_variable_name }
+      let(:variable_name) { 'SOME_VAR' }
+      let(:remote_env)    { { 'SOME_VAR' => 'SOME_VALUE' } }
       subject(:has_env)   { HasEnv.new(env, variable_name) }
 
       it_behaves_like 'test'
 
+      before do
+        allow(env.remote).to receive(:environment) { remote_env }
+      end
+
       describe '#verify' do
-        let(:environment) { double 'environment' }
+        context 'remote environment var is defined' do
+          it 'returns true' do
+            expect(has_env.verify).to be true
+          end
 
-        before do
-          allow(env.remote).to receive(:environment) { environment }
+          context 'when var name is given as a lowercase symbol' do
+            let(:variable_name) { :some_var }
+
+            it 'returns true' do
+              expect(has_env.verify).to be true
+            end
+          end
         end
 
-        it 'stringifies the queried variable name' do
-          expect(environment).to receive(:key?).with(kind_of(String))
-          has_env.verify
-        end
+        context 'remote environment var is not defined' do
+          let(:variable_name) { 'SOME_NON_EXISTENT_VAR' }
 
-        it 'upcases the queried variable name' do
-          expect(environment).to receive(:key?).with('SOME_VARIABLE_NAME')
-          has_env.verify
-        end
-
-        it 'returns true when remote environment var is defined' do
-          allow(environment).to receive(:key?) { true }
-          expect(has_env.verify).to be true
-        end
-
-        it 'returns false when remote environment var is not defined' do
-          allow(environment).to receive(:key?) { false }
-          expect(has_env.verify).to be false
+          it 'returns false' do
+            expect(has_env.verify).to be false
+          end
         end
       end
     end

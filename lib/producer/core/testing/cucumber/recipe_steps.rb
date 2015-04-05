3d2b798aac7bda3487f7d67dbd4ea377e5c22343
Thibault Jouan <tj@a13.fr>
2015-04-05 08:07:47 +0000

    Fix cucumber recipe steps

--- lib/producer/core/testing/cucumber/recipe_steps.rb
+++ lib/producer/core/testing/cucumber/recipe_steps.rb
@@ -57,11 +57,11 @@ When /^I successfully execute the recipe on remote target$/ do
   run_recipe remote: true, check: true
 end
 
-When /^I successfully execute the recipe on remote target with option? (-.+)$/ do |options|
+When /^I successfully execute the recipe on remote target with options? (-.+)$/ do |options|
   run_recipe remote: true, options: options, check: true
 end
 
-When /^I successfully execute the recipe with option? (-.+)$/ do |options|
+When /^I successfully execute the recipe with options? (-.+)$/ do |options|
   run_recipe options: options, check: true
 end
 

16651d39eb25d1cbffbee4467b19bc00320cf999
Thibault Jouan <tj@a13.fr>
2015-07-14 11:12:08 +0000

    Simplify MockRemote#execute

--- lib/producer/core/testing/mock_remote.rb
+++ lib/producer/core/testing/mock_remote.rb
@@ -7,31 +7,33 @@ module Producer
         end
 
         def execute command, output = '', error_output = ''
-          tokens = command.gsub(/\d?>.*/, '').split
-          program = tokens.shift
-
-          case program
-          when 'echo'
-            out = tokens.join(' ') << "\n"
-            if command =~ />&2\z/
-              error_output << out
-            else
-              output << out
-            end
-          when 'true'
-            output << ''
-          when 'false'
-            fail RemoteCommandExecutionError
-          when 'type'
-            fail RemoteCommandExecutionError unless %w[
-              echo
-              true
-              false
-              type
-            ].include? tokens.first
-          end
-
+          program, *args = command.gsub(/\d?>.*/, '').split
+          program_output = command =~ />&2\z/ ? error_output : output
+          send "handle_program_#{program}", args, program_output
           output
+        end
+
+      private
+
+        def handle_program_echo args, output
+          output << args.join(' ') << "\n"
+        end
+
+        def handle_program_true _, output
+          output << ''
+        end
+
+        def handle_program_false *_
+          fail RemoteCommandExecutionError
+        end
+
+        def handle_program_type args, _
+          fail RemoteCommandExecutionError unless %w[
+            echo
+            true
+            false
+            type
+          ].include? args.first
         end
       end
     end
